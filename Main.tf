f4678696daaa6c8d8de9ee33037d2125fd235be7
Ravinder Kumar <ravinderkumarzte@gmail.com>
2026-07-27 00:39:05 +0530

    out

--- Main.tf
+++ Main.tf
@@ -12,3 +12,9 @@ variable "resource_group_name" {
   description = "The name of the resource group"
   type        = string                
 }
+
+variable "location" {
+  description = "The location of the resource group"
+  type        = string
+  default     = "East US"
+}
